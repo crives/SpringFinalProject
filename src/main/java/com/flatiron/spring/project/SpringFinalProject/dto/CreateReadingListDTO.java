6c450f6303ca4f724096cad4a1b4bfd7dc1acee6
Caroline Rives <car.rives@gmail.com>
2022-08-14 18:06:03 -0400

    Removed unused DTOs

--- src/main/java/com/flatiron/spring/project/SpringFinalProject/dto/CreateReadingListDTO.java
+++ src/main/java/com/flatiron/spring/project/SpringFinalProject/dto/CreateReadingListDTO.java
@@ -15,6 +15,5 @@ public class CreateReadingListDTO {
 
     @NotBlank
     private String name;
-
     private List<BookDTO> books;
 }
--- src/main/java/com/flatiron/spring/project/SpringFinalProject/dto/ReadingListResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.flatiron.spring.project.SpringFinalProject.dto;
-
-import lombok.Data;
-
-@Data
-public class ReadingListResponseDTO {
-}
